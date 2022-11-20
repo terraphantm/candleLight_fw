ba5f30e22e8f5ed47229c1c2fe84a39136330678
Marc Kleine-Budde <mkl@blackshift.org>
2022-11-20 20:33:21 +0100

    compiler.h: add IS_ENABLED and friends
    Import from FreeBSD.

--- include/compiler.h
+++ include/compiler.h
@@ -85,4 +85,33 @@
 		_t _n[0]; \
 	}
 
+/*
+ * Checking if an option is defined would be easy if we could do CPP inside CPP.
+ * The defined case whether -Dxxx or -Dxxx=1 are easy to deal with.  In either
+ * case the defined value is "1". A more general -Dxxx=<c> case will require
+ * more effort to deal with all possible "true" values. Hope we do not have
+ * to do this as well.
+ * The real problem is the undefined case.  To avoid this problem we do the
+ * concat/varargs trick: "yyy" ## xxx can make two arguments if xxx is "1"
+ * by having a #define for yyy_1 which is "ignore,".
+ * Otherwise we will just get "yyy".
+ * Need to be careful about variable substitutions in macros though.
+ * This way we make a (true, false) problem a (don't care, true, false) or a
+ * (don't care true, false).  Then we can use a variadic macro to only select
+ * the always well known and defined argument #2.  And that seems to be
+ * exactly what we need.  Use 1 for true and 0 for false to also allow
+ * #if IS_*() checks pre-compiler checks which do not like #if true.
+ */
+#define ___XAB_1 dontcare,
+#define ___IS_XAB(_ignore, _x, ...) (_x)
+#define __IS_XAB(_x)				___IS_XAB(_x 1, 0)
+#define _IS_XAB(_x)					__IS_XAB(__CONCAT(___XAB_, _x))
+
+/* This is if CONFIG_ccc=y. */
+#define IS_BUILTIN(_x) _IS_XAB(_x)
+/* This is if CONFIG_ccc=m. */
+#define IS_MODULE(_x)  _IS_XAB(_x ## _MODULE)
+/* This is if CONFIG_ccc is compiled in(=y) or a module(=m). */
+#define IS_ENABLED(_x) (IS_BUILTIN(_x) || IS_MODULE(_x))
+
 #endif /* _LINUXKPI_LINUX_COMPILER_H_ */
