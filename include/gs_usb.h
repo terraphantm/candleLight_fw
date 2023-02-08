terraphantm/candleLight_fw

commit ad2f13ace724736d734311a231bb04be22c29751

gs_usb.h: document GS_CAN_MODE_BERR_REPORTING
In commit
https://github.com/torvalds/linux/commit/1f1835264d81da03fcd05646542a622678b379d6
[1] Bus Error Reporting has been added to the Linux driver. Document
this bit here accordingly.

The new feature ("GS_CAN_FEATURE_BERR_REPORTING") indicates that the
bus error reporting in the CAN controller can switched on and off with
the GS_CAN_MODE_BERR_REPORTING mode bit in the GS_USB_BREQ_MODE
control message.

[1] https://git.kernel.org/torvalds/c/1f1835264d81da03fcd05646542a622678b379d6

include/gs_usb.h
@@ -49,6 +49,7 @@ THE SOFTWARE.
 /* #define GS_CAN_FEATURE_REQ_USB_QUIRK_LPC546XX (1<<9) */
 /* #define GS_CAN_FEATURE_BT_CONST_EXT          (1<<10) */
 /* #define GS_CAN_FEATURE_TERMINATION           (1<<11) */
+#define GS_CAN_MODE_BERR_REPORTING (1<<12)
 
 #define GS_CAN_FEATURE_LISTEN_ONLY				(1<<0)
 #define GS_CAN_FEATURE_LOOP_BACK				(1<<1)
@@ -74,6 +75,7 @@ THE SOFTWARE.
  * - struct gs_device_termination_state
  */
 #define GS_CAN_FEATURE_TERMINATION				(1<<11)
+#define GS_CAN_FEATURE_BERR_REPORTINGBIT		(1<<12)
 
 #define GS_CAN_FLAG_OVERFLOW					(1<<0)
 #define GS_CAN_FLAG_FD							(1<<1) /* is a CAN-FD frame */

commit 62a4b4a02a7c3d44e51cc049ad1d6ce1425243a5

gs_usb.h: fix typo in GS_CAN_FEATURE_BERR_REPORTING
Fixes: ad2f13ace724 ("gs_usb.h: document GS_CAN_MODE_BERR_REPORTING")

include/gs_usb.h
@@ -77,7 +77,7 @@ THE SOFTWARE.
  * - struct gs_device_termination_state
  */
 #define GS_CAN_FEATURE_TERMINATION				(1<<11)
-#define GS_CAN_FEATURE_BERR_REPORTINGBIT		(1<<12)
+#define GS_CAN_FEATURE_BERR_REPORTING			(1<<12)
 #define GS_CAN_FEATURE_GET_STATE				(1<<13)
 
 #define GS_CAN_FLAG_OVERFLOW					(1<<0)